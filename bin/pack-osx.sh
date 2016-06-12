87a96864d982ec80c3c4cc7cf2cec92267a9c97f
emsk <emsk1987@gmail.com>
2016-06-12 09:48:09 +0900

    Upgrade electron-builder dependency to ~3.27.0

--- bin/pack-osx.sh
+++ bin/pack-osx.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 $(npm bin)/build --platform=darwin --arch=x64 --dist
-mv './dist/Redmine Notifier-darwin-x64/Redmine Notifier-0.5.0.dmg' './dist/Redmine Notifier-darwin-x64/RedmineNotifierSetup.dmg'
+mv './dist/osx/Redmine Notifier-0.5.0.dmg' ./dist/osx/RedmineNotifierSetup.dmg
