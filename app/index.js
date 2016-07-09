cca215a909f7a876ba6f8514e0ec23a8ed1c573e
emsk <emsk1987@gmail.com>
2016-07-09 13:26:33 +0900

    Use default `label` and `accelerator` options in `MenuItem`

--- app/index.js
+++ app/index.js
@@ -55,12 +55,12 @@
         {
           label: 'Edit',
           submenu: [
-            { label: 'Undo',       accelerator: 'CmdOrCtrl+Z',       role: 'undo' },
-            { label: 'Redo',       accelerator: 'Shift+CmdOrCtrl+Z', role: 'redo' },
-            { label: 'Cut',        accelerator: 'CmdOrCtrl+X',       role: 'cut' },
-            { label: 'Copy',       accelerator: 'CmdOrCtrl+C',       role: 'copy' },
-            { label: 'Paste',      accelerator: 'CmdOrCtrl+V',       role: 'paste' },
-            { label: 'Select All', accelerator: 'CmdOrCtrl+A',       role: 'selectall' }
+            { role: 'undo' },
+            { role: 'redo' },
+            { role: 'cut' },
+            { role: 'copy' },
+            { role: 'paste' },
+            { role: 'selectall' }
           ]
         }
       ]);
