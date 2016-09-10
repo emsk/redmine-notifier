54e3fcc69374388cb35967f4678687d915fe7a36
emsk <emsk1987@gmail.com>
2016-09-10 19:08:40 +0900

    Add version to suffix of installer filename

--- bin/pack-win.sh
+++ bin/pack-win.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 $(npm bin)/build --win --x64
-mv './dist/win/Redmine Notifier Setup 0.6.0.exe' ./dist/win/RedmineNotifierSetup.exe
+mv './dist/win/Redmine Notifier Setup 0.6.0.exe' ./dist/win/RedmineNotifierSetup-0.6.0.exe
