c2d24b54909939c0fc3469caa82d919b2b5114a8
emsk <emsk1987@gmail.com>
2017-03-09 22:42:16 +0900

    Update electron-builder dependency to 15.2.0

--- bin/pack-mac.sh
+++ bin/pack-mac.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd $(dirname $0)/..
 $(yarn bin)/build --mac --x64
-mv './dist/mac/Redmine Notifier-0.8.0.dmg' ./dist/mac/RedmineNotifierSetup-0.8.0.dmg
+mv './dist/Redmine Notifier-0.8.0.dmg' ./dist/mac/RedmineNotifierSetup-0.8.0.dmg
